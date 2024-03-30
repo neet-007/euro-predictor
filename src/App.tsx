4cc70731a945d6346054878d7e126c8670056a8a
neet-007 <moayedeffect@gmail.com>
2024-03-30 05:42:05 +0300

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { ComponentProps, useEffect, useState } from "react"
-import { useEuroStore, setRoundOf16, addThirdPlaces } from "./store"
+import { useEuroStore, setRoundOf16, addThirdPlaces, ROUNDOF16MATCHES, RoundOf16Key, setQuarterFinal, QuarterFinalsKey, semiFinalsKey, FinalKey, setSemiFinal, setFinal, QUARTERFINALSMATCHES, SEMIFINALSMATCHES, FINALMATCHES, EuroStoreInterface, setWinner } from "./store"
 
 interface GroupCardProps extends ComponentProps<'article'>{
   group:string[]
@@ -29,7 +29,7 @@ const GroupCard:React.FC<GroupCardProps> = ({group, groupName}) => {
       return [...prev]
     })
     setLastChose(prev => {
-      if(prev === 2) return 3
+      if(prev === 3) return 3
       return prev + 1
     })
     setOldGroup(prev => {
@@ -62,17 +62,28 @@ const ThirdPlaces:React.FC<ThirdPlacesProps> = () => {
   const [orderdGrouo, setOrderdGroup] = useState<string[]>(['', '', '', '', '', ''])
   const [lastChosen, setLastChose] = useState<number>(0)
 
+  useEffect(() => {
+    if(lastChosen === 4){
+      for(let i = 0; i < 4; i++){
+        const key = `T${i + 1}` as RoundOf16Key
+        setRoundOf16(key, orderdGrouo[i])
+      }
+    }
+  },[lastChosen])
+
   function choose(nation:string){
     if(oldGroup.length === 0) return
     setOrderdGroup(prev => {
       prev[lastChosen] = nation
-      if(lastChosen === 4){
-        prev[5] = oldGroup.find(x => x!== nation)!
+      if(lastChosen === 4 && oldGroup.length === 6){
+          console.log(prev)
+          console.log(oldGroup.find(x => !prev.includes(x)))
+        prev[5] = oldGroup.find(x => !prev.includes(x))!
       }
       return [...prev]
     })
     setLastChose(prev => {
-      if(prev === 4) return 4
+      if(prev === 5) return 5
       return prev + 1
     })
   }
@@ -93,18 +104,77 @@ const ThirdPlaces:React.FC<ThirdPlacesProps> = () => {
   )
 }
 
+interface KnockOutProps extends ComponentProps<'article'>{
+  round: 'Q' | 'S' | 'F' | 'W'
+  MATCHES: typeof ROUNDOF16MATCHES | typeof QUARTERFINALSMATCHES | typeof SEMIFINALSMATCHES | typeof FINALMATCHES
+  roundGroup: any
+}
+
+const KnockOut:React.FC<KnockOutProps> = ({round, MATCHES, roundGroup}) => {
+  const roundOf16 = useEuroStore(store => store.roundOf16)
+
+  function quailfy(index:any, value:any){
+    if(round === 'Q'){
+      let key = '' as QuarterFinalsKey;
+      if(index === 'A1' || index === 'C1') key = 'Q1'
+      else if(index === 'A2' || index === 'B2')key = 'Q2'
+      else if(index === 'B1' || index === 'T1')key = 'Q3'
+      else if(index === 'C2' || index === 'T4')key = 'Q4'
+      else if(index === 'F1' || index === 'T3')key = 'Q5'
+      else if(index === 'D2' || index === 'E2')key = 'Q6'
+      else if(index === 'E1' || index === 'T2')key = 'Q7'
+      else if(index === 'D1' || index === 'F2')key = 'Q8'
+      setQuarterFinal(key, value)
+    }
+   if(round === 'S'){
+    let key = '' as semiFinalsKey
+    if(index === 'Q1' || index === 'Q3') key = 'S1'
+    else if(index === 'Q2' || index === 'Q4') key = 'S2'
+    else if(index === 'Q5' || index === 'Q6') key = 'S3'
+    else if(index === 'Q7' || index === 'Q8') key = 'S4'
+    setSemiFinal(key, value)
+   }
+   if(round === 'F'){
+    let key = '' as FinalKey
+    if(index === 'S1' || index === 'S2') key = 'F1'
+    else if(index === 'S3' || index === 'S4') key = 'F2'
+    setFinal(key, value)
+   }
+   if(round === 'W'){
+    setWinner(value)
+   }
+  }
+
+  return (
+    <>
+    {Object.keys(MATCHES).map((team) => {
+      return <div className=" flex gap-4">
+              <button className=" bg-red-400 h-[2rem]" onClick={() => quailfy(team, roundGroup[team])}>
+              {roundGroup[team]}
+              </button>
+              <button className=" bg-red-400 h-[2rem]" onClick={() => quailfy(MATCHES[team], roundGroup[MATCHES[team]])}>
+              {roundGroup[MATCHES[team]]}
+              </button>
+             </div>
+    })}
+    </>
+  )
+}
+
 function App() {
-  const [A, setA] = useState<string[]>(['Germany', 'Scotland', 'Hungary', 'Switzerland'])
-  const [B, setB] = useState<string[]>(['Spain', 'Croatia', 'Italy', 'Albania'])
-  const [C, setC] = useState<string[]>(['Slovenia', 'Denmark', 'Serbia', 'England'])
-  const [D, setD] = useState<string[]>(['Poland', 'Netherlands', 'Austria', 'France'])
-  const [E, setE] = useState<string[]>(['Belgium', 'Slovakia', 'Romania', 'Ukraine'])
-  const [F, setF] = useState<string[]>(['Turkey', 'Georgia', 'Portugal', 'Czech'])
+  const A = ['Germany', 'Scotland', 'Hungary', 'Switzerland']
+  const B = ['Spain', 'Croatia', 'Italy', 'Albania']
+  const C = ['Slovenia', 'Denmark', 'Serbia', 'England']
+  const D = ['Poland', 'Netherlands', 'Austria', 'France']
+  const E = ['Belgium', 'Slovakia', 'Romania', 'Ukraine']
+  const F = ['Turkey', 'Georgia', 'Portugal', 'Czech']
 
-  const thirdPlaces = useEuroStore(store => store.thirdPlaces)
-
-  console.log(useEuroStore(store => store.thirdPlaces))
-  console.log(useEuroStore(store => store.roundOf16))
+  const roundOf16 = useEuroStore(store => store.roundOf16)
+  const quarterFinals = useEuroStore(store => store.quarterFinals)
+  const semiFinal = useEuroStore(store => store.semiFinals)
+  const final = useEuroStore(store => store.final)
+  const winner = useEuroStore(store => store.winner)
+  console.log(final)
 
   function swapTeams(fromIndex:number, toIndex:number, setFunction:React.Dispatch<React.SetStateAction<string[]>>){
     setFunction(prev =>{
@@ -125,6 +195,13 @@ function App() {
       <GroupCard group={E} groupName="E" />
       <GroupCard group={F} groupName="F" />
       <ThirdPlaces/>
+      <KnockOut round="Q" MATCHES={ROUNDOF16MATCHES} roundGroup={roundOf16}/>
+      <KnockOut round="S" MATCHES={QUARTERFINALSMATCHES} roundGroup={quarterFinals}/>
+      <KnockOut round="F" MATCHES={SEMIFINALSMATCHES} roundGroup={semiFinal}/>
+      <KnockOut round="W" MATCHES={FINALMATCHES} roundGroup={final}/>
+      <div>
+        {winner}
+      </div>
     </div>
   )
 }
--- src/store.ts
+++ src/store.ts
@@ -2,17 +2,29 @@ import { create } from "zustand";
 
 //type thirdPlacesKey = 0 | 1 | 2 | 3 | 4 | 5
 
-type RoundOf16Key = 'A1' | 'A2' | 'B1' | 'B2' | 'C1' | 'C2' | 'D1' | 'D2' | 'E1' | 'E2' | 'F1' | 'F2' | 'T1' | 'T2' | 'T3' | 'T4';
+export type RoundOf16Key = 'A1' | 'A2' | 'B1' | 'B2' | 'C1' | 'C2' | 'D1' | 'D2' | 'E1' | 'E2' | 'F1' | 'F2' | 'T1' | 'T2' | 'T3' | 'T4';
 
+export type QuarterFinalsKey = 'Q1' | 'Q2' | 'Q3' | 'Q4' | 'Q5' | 'Q6' | 'Q7' | 'Q8'
 
-interface EuroStoreInterface {
+export type semiFinalsKey = 'S1' | 'S2' | 'S3' | 'S4'
+
+export type FinalKey = 'F1' | 'F2'
+
+export interface EuroStoreInterface {
     thirdPlaces:(string)[]
     roundOf16:{
         [key in RoundOf16Key]: string | undefined;
     }
-    quarterFinals:any
-    semiFinals:any
-    final:any
+    quarterFinals:{
+        [key in QuarterFinalsKey]:string | undefined
+    }
+    semiFinals:{
+        [key in semiFinalsKey]:string | undefined
+    }
+    final:{
+        [key in FinalKey]:string | undefined
+    }
+    winner:string | undefined
 }
 
 export const useEuroStore = create<EuroStoreInterface>(() =>({
@@ -35,9 +47,27 @@ export const useEuroStore = create<EuroStoreInterface>(() =>({
         T3:undefined,
         T4:undefined
     },
-    quarterFinals:undefined,
-    semiFinals:undefined,
-    final:undefined
+    quarterFinals:{
+        Q1:undefined,
+        Q2:undefined,
+        Q3:undefined,
+        Q4:undefined,
+        Q5:undefined,
+        Q6:undefined,
+        Q7:undefined,
+        Q8:undefined,
+    },
+    semiFinals:{
+        S1:undefined,
+        S2:undefined,
+        S3:undefined,
+        S4:undefined
+    },
+    final:{
+        F1:undefined,
+        F2:undefined
+    },
+    winner:undefined
 }))
 
 export const addThirdPlaces = (value:string) => useEuroStore.setState(state => ({thirdPlaces:[...state.thirdPlaces, value]}))
@@ -45,3 +75,38 @@ export const addThirdPlaces = (value:string) => useEuroStore.setState(state => (
 export const removeThirdPlaces = (value:string) => useEuroStore.setState(state => ({thirdPlaces:state.thirdPlaces.filter(x => x !== value)}))
 
 export const setRoundOf16 = (key:RoundOf16Key, value:string) => useEuroStore.setState(state => ({roundOf16:{...state.roundOf16, [key]:value}}))
+
+export const setQuarterFinal = (key:QuarterFinalsKey, value:string) => useEuroStore.setState(state => ({quarterFinals:{...state.quarterFinals, [key]:value}}))
+
+export const setSemiFinal = (key:semiFinalsKey, value:string) => useEuroStore.setState(state => ({semiFinals:{...state.semiFinals, [key]:value}}))
+
+export const setFinal = (key:FinalKey, value:string) => useEuroStore.setState(state => ({final:{...state.final, [key]:value}}))
+
+export const setWinner = (value:string) => useEuroStore.setState(()=> ({winner:value}))
+
+export const ROUNDOF16MATCHES = {
+    A1:'C1',
+    A2:'B2',
+    B1:'T1',
+    C2:'T4',
+    F1:'T3',
+    D2:'E2',
+    E1:'T2',
+    D1:'F2',
+}
+
+export const QUARTERFINALSMATCHES = {
+    Q1:'Q3',
+    Q2:'Q4',
+    Q5:'Q6',
+    Q7:'Q8',
+}
+
+export const SEMIFINALSMATCHES = {
+    S1:'S2',
+    S3:'S4',
+}
+
+export const FINALMATCHES = {
+    F1:'F2'
+}
